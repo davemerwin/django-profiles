a4aef976d20d91c2ffd20646e0ea3ff185fe13d0
Dave Merwin <dave@davemerwin.com>
2010-12-04 20:33:40 -0800

    updating changelog etc

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from distutils.core import setup
 
 setup(name='django-profiles',
-      version='0.2',
+      version='0.3',
       description='User-profile application for Django',
-      author='James Bennett',
-      author_email='james@b-list.org',
-      url='http://www.bitbucket.org/ubernostrum/django-profiles/wiki/',
-      download_url='http://www.bitbucket.org/ubernostrum/django-profiles/get/v0.2.gz',
+      author='James Bennett, Dave Merwin',
+      author_email='james@b-list.org, dave@davemerwin.com',
+      url='https://github.com/davemerwin/django-profiles',
+      download_url='https://github.com/davemerwin/django-profiles/archives/master',
       packages=['profiles'],
       classifiers=['Development Status :: 4 - Beta',
                    'Environment :: Web Environment',
